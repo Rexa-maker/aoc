221968a3ff2a6f4e738b3d723b81079c6a77af4c
rexa <rexamaker@gmail.com>
2023-05-24 13:02:19 -0700

    2015 day01 star2

--- 2015/day01/src/main.rs
+++ 2015/day01/src/main.rs
@@ -5,25 +5,63 @@ fn main() {
 }
 
 fn solver(input: &str) -> String {
-    what_floor(input).to_string()
+    format!("{:?} {:?}", what_floor(input), basement(input))
+}
+
+struct Solver<'a> {
+    remainder: Option<&'a str>,
+    current_floor: i32,
+}
+
+impl Solver<'_> {
+    fn new<'a>(input: &'a str) -> Solver {
+        Solver {
+            remainder: Some(input),
+            current_floor: 0,
+        }
+    }
+}
+
+impl Iterator for Solver<'_> {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        fn char_to_increment(c: char) -> i32 {
+            match c {
+                '(' => 1,
+                ')' => -1,
+                _ => 0,
+            }
+        }
+
+        if let Some(remainder) = self.remainder {
+            if remainder.len() == 1 {
+                self.remainder = None;
+            } else {
+                self.remainder = Some(remainder.split_at(1).1);
+            }
+            self.current_floor += char_to_increment(remainder.chars().next().unwrap());
+            Some(self.current_floor)
+        } else {
+            None
+        }
+    }
 }
 
 fn what_floor(input: &str) -> i32 {
-    let mut floor = 0;
+    *Solver::new(input).collect::<Vec<i32>>().last().unwrap()
+}
 
-    for a in input.chars() {
-        match a {
-            '(' => {
-                floor = floor + 1;
-            }
-            ')' => {
-                floor = floor - 1;
-            }
-            _ => {}
+fn basement(input: &str) -> u32 {
+    let mut basement: u32 = 0;
+    for next in Solver::new(input) {
+        if next == -1 {
+            return basement + 1;
+        } else {
+            basement += 1;
         }
     }
-
-    floor
+    0
 }
 
 #[test]
@@ -40,4 +78,7 @@ fn examples() {
 
     assert_eq!(-3, what_floor(")))"));
     assert_eq!(-3, what_floor(")())())"));
+
+    assert_eq!(1, basement(")"));
+    assert_eq!(5, basement("()())"));
 }
