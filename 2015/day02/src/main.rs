63dfd075e32e6cce5a877f4b2afa690dbbb3be66
rexa <rexamaker@gmail.com>
2023-05-25 19:59:36 -0700

    day2 star2

--- 2015/day02/src/main.rs
+++ 2015/day02/src/main.rs
@@ -1,11 +1,20 @@
 fn main() {
-    println!("{}", tally_surfaces(include_str!("input")));
+    let input = include_str!("input");
+    println!("{} {}", tally_surfaces(input), tally_ribbon(input));
 }
 
 fn tally_surfaces(input: &str) -> u32 {
     let mut tally = 0;
     for line in input.lines() {
         tally += Box::new(line).total_needed();
+    }
+    tally
+}
+
+fn tally_ribbon(input: &str) -> u32 {
+    let mut tally = 0;
+    for line in input.lines() {
+        tally += Box::new(line).ribbon_feet();
     }
     tally
 }
@@ -35,6 +44,13 @@ impl Box {
         sorted.sort();
         self.surface() + sorted[0] * sorted[1]
     }
+
+    fn ribbon_feet(self) -> u32 {
+        let mut sorted = vec![self.l, self.w, self.h];
+        sorted.sort();
+
+        2 * (sorted[0] + sorted[1]) + self.l * self.w * self.h
+    }
 }
 
 #[test]
@@ -43,4 +59,7 @@ fn examples() {
     assert_eq!(Box::new("2x3x4").total_needed(), 58);
     assert_eq!(Box::new("1x1x10").surface(), 42);
     assert_eq!(Box::new("1x1x10").total_needed(), 43);
+
+    assert_eq!(Box::new("2x3x4").ribbon_feet(), 34);
+    assert_eq!(Box::new("1x1x10").ribbon_feet(), 14);
 }
