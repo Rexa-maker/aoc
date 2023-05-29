a3251ec4b53bffe1a8a762c3189f68301bb80c63
rexa <rexamaker@gmail.com>
2023-05-29 07:52:43 -0700

    2017 day01 star02

--- 2017/day01/src/main.rs
+++ 2017/day01/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
-    println!("{}", double_sum(include_str!("input")));
+    let input = include_str!("input");
+    println!("{} {}", double_sum(input), halfway_sum(input));
 }
 
 fn double_sum(input: &str) -> u32 {
@@ -26,10 +27,28 @@ fn double_sum(input: &str) -> u32 {
     tally
 }
 
+fn halfway_sum(input: &str) -> u32 {
+    let half = input.len() / 2;
+    let digits: Vec<u32> = input.chars().map(|c| c.to_digit(10).unwrap()).collect();
+    let mut tally = 0;
+    for i in 0..half {
+        if digits[i] == digits[i + half] {
+            tally += digits[i] * 2;
+        }
+    }
+    tally
+}
+
 #[test]
 fn example() {
     assert_eq!(double_sum("1122"), 3);
     assert_eq!(double_sum("1111"), 4);
     assert_eq!(double_sum("1234"), 0);
     assert_eq!(double_sum("91212129"), 9);
+
+    assert_eq!(halfway_sum("1212"), 6);
+    assert_eq!(halfway_sum("1221"), 0);
+    assert_eq!(halfway_sum("123425"), 4);
+    assert_eq!(halfway_sum("123123"), 12);
+    assert_eq!(halfway_sum("12131415"), 4);
 }
