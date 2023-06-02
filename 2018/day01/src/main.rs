196476f304f2fef8e06a7630af7b7c1ec231afd9
rexa <rexamaker@gmail.com>
2023-06-02 14:23:15 -0700

    added first example

--- 2018/day01/src/main.rs
+++ 2018/day01/src/main.rs
@@ -9,6 +9,7 @@ fn frequency(input: &str) -> i32 {
 
 #[test]
 fn examples() {
+    assert_eq!(frequency("+1\n-2\n+3\n+1"), 3);
     assert_eq!(frequency("+1\n+1\n+1"), 3);
     assert_eq!(frequency("+1\n+1\n-2"), 0);
     assert_eq!(frequency("-1\n-2\n-3"), -6);
