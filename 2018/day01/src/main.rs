c6c93a82e13ee5b4b749c18a09ac1d61086ff3ef
rexa <rexamaker@gmail.com>
2023-06-01 17:33:04 -0700

    2018 day01 star1

---
+++ 2018/day01/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let input = include_str!("input");
+    println!("{}", frequency(input));
+}
+
+fn frequency(input: &str) -> i32 {
+    input.lines().map(|val| {val.parse().unwrap()}).fold(0, |tally, x: i32| tally + x)
+}
+
+#[test]
+fn examples() {
+    assert_eq!(frequency("+1\n+1\n+1"), 3);
+    assert_eq!(frequency("+1\n+1\n-2"), 0);
+    assert_eq!(frequency("-1\n-2\n-3"), -6);
+}
