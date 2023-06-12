773cd8f896b60bc29a2ec352305df846b88267a7
rexa <rexamaker@gmail.com>
2023-06-12 10:00:30 -0700

    ugh

--- 2015/day09/src/main.rs
+++ 2015/day09/src/main.rs
@@ -1,12 +1,39 @@
 use lazy_static::lazy_static;
-use petgraph::algo::dijkstra;
-use petgraph::graph::{NodeIndex, UnGraph};
 use regex::Regex;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 fn main() {
     let input = include_str!("input");
     println!("{}", shortest_distance(input));
+}
+
+// ChatGPT
+fn tsp(graph: &HashMap<(String, String), usize>, current: &String, visited: &Vec<String>) -> usize {
+    // Base case: all locations have been visited
+    if visited.len() == graph.len() {
+        return 0;
+    }
+
+    // Recursive case: find the shortest path from the current location
+    let mut shortest = std::usize::MAX;
+    for ((a, b), distance) in graph
+        .iter()
+        .filter(|((a, b), _)| a == current || b == current)
+    {
+        let next = if a == current { b } else { a };
+        if visited.contains(&next) {
+            continue;
+        }
+
+        let mut new_visited = visited.clone();
+        new_visited.push(next.clone());
+        let path_len = distance + tsp(graph, next, &new_visited);
+        if path_len < shortest {
+            shortest = path_len;
+        }
+    }
+
+    shortest
 }
 
 fn shortest_distance(input: &str) -> usize {
@@ -15,16 +42,12 @@ fn shortest_distance(input: &str) -> usize {
             Regex::new(r"(?P<source>\w+) to (?P<destination>\w+) = (?P<distance>\d+)").unwrap();
     }
 
-    let mut shortest = usize::MAX;
-
-    let mut hash: HashMap<String, NodeIndex> = HashMap::new();
-    let mut graph: UnGraph<String, usize> = UnGraph::new_undirected();
+    let mut graph: HashMap<(String, String), usize> = HashMap::new();
+    let mut cities: HashSet<String> = HashSet::new();
 
     for capture in RE.captures_iter(input) {
-        let names = vec![
-            capture.name("source").unwrap().as_str().to_string(),
-            capture.name("destination").unwrap().as_str().to_string(),
-        ];
+        let src = capture.name("source").unwrap().as_str().to_string();
+        let dst = capture.name("destination").unwrap().as_str().to_string();
         let distance = capture
             .name("distance")
             .unwrap()
@@ -32,30 +55,18 @@ fn shortest_distance(input: &str) -> usize {
             .parse::<usize>()
             .unwrap();
 
-        let mut nodes = Vec::<NodeIndex>::new();
-        for name in names {
-            println!("DEBUGAX {name} {:?}", hash);
-            if hash.contains_key(&name) {
-                nodes.push(*hash.get(&name).unwrap());
-            } else {
-                let index = graph.add_node(name.clone());
-                hash.insert(name, index);
-                nodes.push(index);
-            }
-        }
-
-        graph.add_edge(*nodes.first().unwrap(), *nodes.last().unwrap(), distance);
+        graph.insert((src.clone(), dst.clone()), distance);
+        cities.insert(src);
+        cities.insert(dst);
     }
 
-    println!("{:?} {:?}", hash, graph);
-    let nodes = hash.into_iter().collect::<Vec<(String, NodeIndex)>>();
-    for i in 0..nodes.len() - 1 {}
-    let distances = dijkstra(&graph, any_node, None, |edge| *edge.weight());
+    let mut shortest = usize::MAX;
 
-    for (_, distance) in distances {
-        println!("DEBUGAX {distance}");
-        if distance > 0 && distance < shortest {
-            shortest = distance;
+    for city in cities {
+        let visited = vec![city.clone()];
+        let trip = tsp(&graph, &city, &visited);
+        if trip < shortest {
+            shortest = trip;
         }
     }
 
