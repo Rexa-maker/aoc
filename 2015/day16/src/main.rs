108194b0a95a042c5031f268849a72d80861943f
rexa <rexamaker@gmail.com>
2023-07-01 10:32:13 -0700

    2015 day16 star1

--- 2015/day16/src/main.rs
+++ 2015/day16/src/main.rs
@@ -1,3 +1,91 @@
+use std::collections::HashMap;
+
+struct Sue {
+    name: u16,
+    coumpounds: HashMap<&'static str, u8>,
+}
+static COUMPOUNDS: [&str; 10] = [
+    "children",
+    "cats",
+    "samoyeds",
+    "pomeranians",
+    "akitas",
+    "vizslas",
+    "goldfish",
+    "trees",
+    "cars",
+    "perfumes",
+];
+
+impl Sue {
+    // Input line example: "Sue 1: goldfish: 6, trees: 9, akitas: 0"
+    fn parse(line: &str) -> Sue {
+        // Returns the name of the coumpound within the static array
+        fn coumpound_valid(name: &str) -> Option<&'static str> {
+            for coumpound in COUMPOUNDS.iter() {
+                if name == *coumpound {
+                    return Some(*coumpound);
+                }
+            }
+            None
+        }
+
+        let (name, coumpounds_str) = line.split_once(": ").unwrap();
+        let sue_name = name[4..].parse().unwrap();
+
+        let mut coumpounds = HashMap::new();
+
+        for compound in coumpounds_str.split(", ") {
+            let mut parts = compound.split(": ");
+            let cmpnd_name = coumpound_valid(parts.next().unwrap()).unwrap();
+            let value = parts.next().unwrap().parse().unwrap();
+
+            coumpounds.insert(cmpnd_name, value);
+        }
+
+        Sue {
+            name: sue_name,
+            coumpounds,
+        }
+    }
+}
+
+// Original Sue the message leads us to find
+fn message() -> Sue {
+    Sue::parse(
+        "Sue 0: children: 3, cats: 7, samoyeds: 2, pomeranians: 3, akitas: 0, \
+               vizslas: 0, goldfish: 5, trees: 3, cars: 2, perfumes: 1",
+    )
+}
+
+fn which_sue(input: &str) -> u16 {
+    let message = message();
+    let mut sues = Vec::new();
+
+    for line in input.lines() {
+        sues.push(Sue::parse(line));
+    }
+
+    for sue in sues {
+        let mut valid = true;
+        for (coumpound, value) in message.coumpounds.iter() {
+            if let Some(sue_value) = sue.coumpounds.get(coumpound) {
+                if *value != *sue_value {
+                    valid = false;
+                    break;
+                }
+            }
+        }
+
+        if valid {
+            return sue.name;
+        }
+    }
+
+    0
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("input");
+    println!("{}", which_sue(input));
 }
